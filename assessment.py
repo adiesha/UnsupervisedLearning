8c32c6171888080e78b771b2257eaaa3efb60869
Adiesha <adiesha@gmail.com>
2020-10-16 00:09:02 -0600

    added assesment metrics purity and silhouette

--- assessment.py
+++ assessment.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -5,6 +6,9 @@ def main():
     data = pd.read_csv('iris.data.result.csv', header=None)
     data[len(data.columns)] = data.index
     assessment(data, 5, 6)
+
+    sill = silhouettecofficient(data, 5, 4)
+    print(sill)
 
 
 def test():
@@ -50,6 +54,60 @@ def purity(data, truthColumn, labelColumn):
     purity = puritySum / data.shape[0]
     print(purity)
     return purity
+
+
+def silhouettecofficient(data, labelColumn, kn):
+    indexColumn = len(data.columns) - 1
+    clusterList = data.groupby(labelColumn - 1)[indexColumn].apply(list).values.tolist()
+
+    uIn = np.zeros(data.shape[0])
+    uOut = np.zeros(data.shape[0])
+    for i in clusterList:
+        for j in i:
+            uIn[j] = calculateUin(j, i, data, kn)
+            uOut[j] = calculateUout(j, i, clusterList, data, kn)
+
+    result = 0
+    for index in range(0, data.shape[0]):
+        result = result + (uOut[index] - uIn[index]) / max(uOut[index], uIn[index])
+
+    result = result / data.shape[0]
+
+    return result
+
+
+def calculateUout(index, cluster, clusterList, data, kn):
+    baseTuple = np.array(data.iloc[index, 0:kn])
+    minDistance = np.inf
+    for k in clusterList:
+        sum = 0
+        if clusterList.index(k) == clusterList.index(cluster):
+            continue
+        else:
+            # calculate the distance sum to this cluster
+            for i in k:
+                tempTuple = np.array(data.iloc[i, 0:kn])
+                sum = sum + np.linalg.norm(baseTuple - tempTuple)
+        result = sum / len(k)
+        if result < minDistance:
+            minDistance = result
+
+    return minDistance
+
+
+def calculateUin(index, cluster, data, kn):
+    baseTuple = np.array(data.iloc[index, 0:kn])
+    sum = 0
+    for i in cluster:
+        if i == index:
+            continue
+        else:
+            tempTuple = np.array(data.iloc[i, 0:kn])
+            sum = sum + np.linalg.norm(baseTuple - tempTuple)
+
+    result = sum / (len(cluster) - 1)
+    print(result)
+    return result
 
 
 if __name__ == "__main__":
