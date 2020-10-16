aead25ccc38520bf6c11e8b6579ea64bd4956f89
Kaveen Liyanage <kaveenliyanage@yahoo.com>
2020-10-16 09:04:06 -0600

    k_mean++ initialization Still have to remove comments Also has to add a voting method for multiple random initiation

--- K_means.py
+++ K_means.py
@@ -7,38 +7,55 @@ import pandas as pd
 from numpy.random import rand
 
 
-def k_means(df,K,epsilon):
+def k_means(df, K, epsilon):
 
     D = df.copy()
     t = 0
-    dim = len(D.columns)
+    dim = len(D.columns)-1
     N_t = len(D.index)
-    print("Number of txn")
-    print (N_t)
-    mu = np.zeros(shape=(K,dim))
+    # print("Number of txn")
+    # print (N_t)
+    print("Number of classes")
+    print(K)
+    mu = np.zeros(shape=(K, dim))
 
+
+    # print("Min Max range of input data")
+    D_min = D.min().values
+    D_max = D.max().values
+    # print(D_min)
+
+    # Initailization according to K-means++
     for k in range(K):
+        if k ==0 :
+            mu[k, :] = D_min[0:dim]+(D_max[0:dim] - D_min[0:dim])*rand(dim)
+        else:
+            min_dist = np.ones(shape=(1, N_t))*100
+            for j in range(N_t):
+                xj = D.iloc[j].values
+                for i in range(k):
+                    err_k = xj[0:dim] - mu[i, :]
+                    dist = np.linalg.norm(err_k, ord=2)**2
+                    min_dist[0, j] = min(min_dist[0, j], dist)
+            j_max = np.argmax(min_dist)
+            xj_max = D.iloc[j_max].values
+            mu[k, :] = xj_max[0:dim]
 
-        mu[k,:]  = rand(4)
-    #      a + (b-a) * random().
 
-
-    print("Mean")
+    print("Initial Mean")
     print(mu)
 
-    print("Min Max range of input data")
-    # D_minmax = pd.Series(index=['min','max'],data=[D.min(),D.max()])
-    D_minmax = [D.min().values,D.max().values]
-    print(D_minmax)
 
-
-
-
-    # mu_new = np.array()
     while True:
+        print("iteration")
         t = t+1
+        print(t)
 
-        C = pd.DataFrame(np.zeros(shape=(N_t, 1)), columns=["Clac Class"])
+        C = pd.DataFrame(np.zeros(shape=(N_t, 1)), columns=["Kmeans_Class"])
+
+        # print("Mu old")
+        mu_old = mu.copy()
+        # print(mu_old)
 
         # Cluster Assignment step
         for j in range(N_t) :
@@ -58,10 +75,10 @@ def k_means(df,K,epsilon):
                 # print("mean value")
                 # print(mu[k])
 
-                err_k = xj[0:dim] - mu[k,:]
+                err_k = xj[0:dim] - mu[k, :]
                 # print("error in k")
                 # print(err_k)
-                res[0,k] = np.linalg.norm(err_k,ord=2)**2
+                res[0, k] = np.linalg.norm(err_k, ord=2)**2
                 # print("residual")
                 # print(res)
 
@@ -69,13 +86,40 @@ def k_means(df,K,epsilon):
             # print("i_star")
             # print(i_star)
 
-            C[j] = i_star
+            C.iloc[j] = i_star
 
-
+        # Centroid update
         for k in range(K):
-            mu[k,:] = mu[k,:]
+            # print("Kth cluster k=")
+            # print(k)
+            idx_k = C.loc[C['Kmeans_Class']==k+1].index
+            # print("Class indexes")
+            # print(idx_k)
 
-        if np.linalg.norm([mu_new - mu], ord='fro',axis=0)<= epsilon:
+            if len(idx_k) != 0:
+                c_k = D.iloc[idx_k.values]
+                # print("C_k ")
+                # print(c_k)
+
+                # print("cluster mean")
+                # print(c_k.mean())
+
+                mu[k, :] = c_k.mean()[0:dim]
+
+        # print("New mean")
+        # print(mu)
+
+        # print("Mean error")
+        # print(np.subtract(mu, mu_old))
+        #
+        # print("Mean error norm")
+        # print(np.linalg.norm(np.subtract(mu, mu_old), ord=2, axis=1)**2)
+        print("SSE")
+        print(sum(np.linalg.norm(np.subtract(mu, mu_old), ord=2, axis=1) ** 2))
+        if sum(np.linalg.norm(np.subtract(mu, mu_old), ord=2, axis=1) ** 2) <= epsilon:
+            print("Number of iterations")
+            print(t)
+            return C
             break
 
 def main():
@@ -90,7 +134,7 @@ def main():
     epsilon = 0.001
 
     print("Calling K means")
-    k_means(df,k,epsilon)
+    k_means(df, k, epsilon)
 
     print("k means done")
 if __name__ == "__main__":
