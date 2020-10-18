a704b6f6441468463be995cf12142d6af938956d
md-sid <siddat.md.123@gmail.com>
2020-10-17 21:47:09 -0600

    New Synthetic Data

--- assessment.py
+++ assessment.py
@@ -3,13 +3,13 @@ import pandas as pd
 
 
 def main():
-    data = pd.read_csv('iris.data.dbscan.result.csv', header=None)
-    data[len(data.columns)] = data.index
-    print("printing assessment metrics of iris data on dbscan")
-    puri = purity(data, 5, 6)
-    print(puri)
-    sill = silhouettecofficient(data, 6, 4)
-    print(sill)
+    # data = pd.read_csv('iris.data.dbscan.result.csv', header=None)
+    # data[len(data.columns)] = data.index
+    # print("printing assessment metrics of iris data on dbscan")
+    # puri = purity(data, 5, 6)
+    # print(puri)
+    # sill = silhouettecofficient(data, 6, 4)
+    # print(sill)
 
     data2 = pd.read_csv('sysnthetic.data.dbscan.result.csv', header=None)
     data2[len(data2.columns)] = data2.index
@@ -38,15 +38,15 @@ def purity(data, truthColumn, labelColumn):
 
     indexColumn = len(data.columns) - 1
     groundTruthList = data.groupby(truthColumn - 1)[indexColumn].apply(list).values.tolist()
-    print(groundTruthList)
+    # print(groundTruthList)
     groundTruthListCount = len(groundTruthList)
-    print(groundTruthListCount)
+    # print(groundTruthListCount)
 
     # create the cluster list
     clusterList = data.groupby(labelColumn - 1)[indexColumn].apply(list).values.tolist()
-    print(clusterList)
+    # print(clusterList)
     clusterListCount = len(groundTruthList)
-    print(clusterListCount)
+    # print(clusterListCount)
 
     puritySum = 0
     for i in clusterList:
